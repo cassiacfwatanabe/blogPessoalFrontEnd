74f04d74291f6992feaa39eea0e718304d1557ff
unknown <you@example.com>
2022-03-01 16:48:02 -0300

    Consumo API entrar e cadastrar

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
+  
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) =>{
+      this.userLogin = resp
+      
+      this.router.navigate(['/inicio'])
+    }, erro =>{
+      if(erro.status == 500 || erro.status == 401){
+        alert('Usuario ou senha estão incorretos!')
+      }
+    })
   }
 
 }
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Observable } from 'rxjs';
+import { UserLogin } from '../model/UserLogin';
+import { User } from '../model/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  entrar(userLogin:UserLogin): Observable<UserLogin>{
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+
+  }
+
+  cadastrar(usuario:User):Observable<User> {
+    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', usuario)
+  }
+  
+}
